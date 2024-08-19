6643ba1536eed5356e6540c3c25633537eb119b0
tej <u23ai109@coed.svnit.ac.in>
2024-08-19 23:36:03 +0530

    Update lab2.1.cpp

--- lab2/lab2.1.cpp
+++ lab2/lab2.1.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 using namespace std;
+
+// creating class with required variables
 class order{
     private:
      int n;
@@ -11,35 +13,41 @@ class order{
      int cost;
 
     public:
+//creating constructor for initialising variables
      order(int n,int m,int p){
         this->n=n;
         this->m=m;
         this->p=p;
      }
 
-     inline int discount(int cost){return (cost+(cost/10));}
+     inline int discount(int cost){return (cost+(cost/10));}//creating inline function that returns the final price
 
+
+//function for calculating total price
      void price(){
-        
+        //declaring price
         price_n=n*1500;
         price_m=m*200;
         price_p=p*450;
+         //calculating price after discount
         price_n=(price_n)-((price_n)/20);
         price_m=(price_m)-((3*price_m)/20);
         price_p=(price_p)-((price_p)/10);
-        cost=price_n+price_m+price_p;
-        cost=discount(cost);
+        cost=price_n+price_m+price_p;//suuming the prices
+        cost=discount(cost);//adding tax and returning final price
      }
 
+//function to display total price of purchase
      void display(){
         cout<<"the total cost of ur purchase is "<<cost<<endl;
      }
 };
 
 int main(){
+    //creating object and passing arguments
     order o1(5,6,7);
-    o1.price();
-    o1.display();
+    o1.price();//calculating price
+    o1.display();//returning the price
 
     return 0;
 }
