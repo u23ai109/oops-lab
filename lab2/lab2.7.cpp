642ea74d8358885fe8434c60fea70eb9a0b408c3
tej <u23ai109@coed.svnit.ac.in>
2024-08-20 00:29:27 +0530

    Update lab2.7.cpp

--- lab2/lab2.7.cpp
+++ lab2/lab2.7.cpp
@@ -1,26 +1,28 @@
 #include<iostream>
 using namespace std;
 
+//creating an class with a pointer and int as members
 class arra{
     int * arr;
     int size;
 
     public:
+//creating a default constructor that initialises size and arr as null
      arra(){
         arr=NULL;
         size=0;
      }
-
+// creating a parameterized constructor 
      arra(int s){
         size=s;
-        arr=new int[size];
+        arr=new int[size];//updating the pointer to array using 'new'
         cout<<"enter elements of array"<<endl;
 
         for(int i=0;i<size;i++){
-            cin>>arr[i];
+            cin>>arr[i];//taking input into array
         }
      }
-
+//function to obtain largest element among elements of array
      int large(){
          int l=arr[0];
         for(int i=0;i<size;i++){
@@ -33,8 +35,8 @@ class arra{
 };
 
 int main(){
-    arra a(5);
-    cout<<"largest element is "<<a.large()<<endl;
+    arra a(5);//creating object that takes an argument 
+    cout<<"largest element is "<<a.large()<<endl;//displaying largest element
 
     return 0;
 }
