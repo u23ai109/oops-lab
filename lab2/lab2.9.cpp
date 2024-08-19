d3b86c62fa614d329fa1222b6609986f9912a434
tej <u23ai109@coed.svnit.ac.in>
2024-08-20 00:32:18 +0530

    Update lab2.9.cpp

--- lab2/lab2.9.cpp
+++ lab2/lab2.9.cpp
@@ -2,32 +2,39 @@
 #include<string.h>
 using namespace std;
 
-class book{
-    string name;
-    int cost;
+// Define a 'book' class to represent a book with a name and cost
+class book {
+    string name; // Name of the book
+    int cost;    // Cost of the book
 
     public:
-     book(string name,int cost){
-        this->name=name;
-        this->cost=cost;
+     // Constructor to initialize the book's name and cost
+     book(string name, int cost) {
+        this->name = name;
+        this->cost = cost;
      }
 
-     friend void compare(book &obj1,book &obj2);
+     // Friend function declaration to allow comparison of two book objects
+     friend void compare(book &obj1, book &obj2);
 };
 
-void compare(book &obj1,book &obj2){
-    if(obj1.cost>obj2.cost){
-        cout<<obj2.name <<" is cheaper"<<endl;
-    }else{
-        cout<<obj1.name <<" is cheaper"<<endl;
+// Friend function definition to compare the cost of two books
+void compare(book &obj1, book &obj2) {
+    // Compare the cost of the two books and print the one that is cheaper
+    if (obj1.cost > obj2.cost) {
+        cout << obj2.name << " is cheaper" << endl;
+    } else {
+        cout << obj1.name << " is cheaper" << endl;
     }
 }
 
-int main(){
-    book b1("harry_potter",7000);
-    book b2("The_Boys",9000);
+int main() {
+    // Create two book objects with names and costs
+    book b1("harry_potter", 7000);
+    book b2("The_Boys", 9000);
 
-    compare(b1,b2);
+    // Compare the two books using the friend function
+    compare(b1, b2);
 
     return 0;
 }
